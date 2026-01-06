9af7e9269f7282eb6054e8eee5c1a66c7d59df24
Provash-roy <provashray94@gmail.com>
2026-01-06 23:36:23 +0600

    Database connected

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({
+  path: "./.env",
+});
+const app = express();
+const PORT = process.env.PORT || 3000;
+const connectDB = require("./db/db");
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to start server:", error);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
