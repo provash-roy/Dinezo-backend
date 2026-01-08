498a62a2d1cb1f59f14860fd646c7fadcd3611b7
Provash-roy <provashray94@gmail.com>
2026-01-08 23:39:54 +0600

    Order model added

---
+++ src/models/order.models.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    items: [
+      {
+        menuItem: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Menu",
+          required: true,
+        },
+        quantity: { type: Number, required: true, default: 1 },
+      },
+    ],
+    totalPrice: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "shipped", "delivered"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+export const Order = mongoose.model("Order", orderSchema);
